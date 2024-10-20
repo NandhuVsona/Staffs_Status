5159e2d1178d1a9d06c6f8e839eedf45a6cc674c
NandhuVsona <naveenvsona@gmail.com>
2024-10-20 07:50:23 +0530

    first commit

--- public/home.js
+++ public/home.js
@@ -1,3 +1,19 @@
+
+      
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+          .then((registration) => {
+              console.log('Service Worker registered with scope:', registration.scope);
+          })
+          .catch((error) => {
+              console.log('Service Worker registration failed:', error);
+          });
+  });
+}
+  
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const isdark = JSON.parse(localStorage.getItem("mode"));
   let container = document.querySelector(".container");
