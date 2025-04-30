5f04b5d97e7d6a8e04071bfe771405798ef6a00b
Devpro <131090906+NandhuVsona@users.noreply.github.com>
2025-04-30 06:21:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -205,7 +205,7 @@ app.get("/api/v1/staffs/send-sms", async (req, res) => {
     const currentMinute = now.getMinutes();
 
     const classTimings = {
-      "04:50": "firstPeriod",
+      [process.env.FIRST_PERIOD_TIME]: "firstPeriod",
       [process.env.SECOND_PERIOD_TIME]: "secondPeriod",
       [process.env.THIRD_PERIOD_TIME]: "thirdPeriod",
       [process.env.FOURTH_PERIOD_TIME]: "fourthPeriod",
